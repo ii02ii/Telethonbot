af85c079e17b1e104c4a7d615724fed1fa13e909
ii02ii <abolove4@yahoo.com>
2021-06-22 16:17:21 +0300

    Add files via upload

---
+++ userbot/plugins/animation5_AR.py
@@ -0,0 +1,234 @@
+
+import asyncio
+
+
+@bot.on(admin_cmd(pattern=f"جاري$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"جاري$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(20)
+    event = await edit_or_reply(event, "▯")
+    animation_chars = ["▮", "▯", "▬", "▭", "‎"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 4])
+
+
+@bot.on(admin_cmd(pattern=f"شباك$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"شباك$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(20)
+    event = await edit_or_reply(event, "◨")
+    animation_chars = ["◧", "◨", "◧", "◨", "‎"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 4])
+
+
+@bot.on(admin_cmd(pattern=f"اعلى$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"اعلى$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(20)
+    event = await edit_or_reply(event, "╻")
+    animation_chars = ["╹", "╻", "╹", "╻", "‎"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 4])
+
+
+@bot.on(admin_cmd(pattern=f"طوبة$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"طوبة$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(20)
+    event = await edit_or_reply(event, "Round...")
+    animation_chars = ["⚫", "⬤", "●", "∘", "‎"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 4])
+
+
+@bot.on(admin_cmd(pattern=f"قلب$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"قلب$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.5
+    animation_ttl = range(20)
+    event = await edit_or_reply(event, "❤️")
+    animation_chars = ["🖤", "❤️", "🖤", "❤️", "‎"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 4])
+
+
+@bot.on(admin_cmd(pattern=f"مكتئب$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"مكتئب$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1
+    animation_ttl = range(20)
+    event = await edit_or_reply(event, "😢")
+    animation_chars = [
+        "😁",
+        "😧",
+        "😡",
+        "😢",
+        "😁",
+        "😧",
+        "😡",
+        "😢",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 10])
+
+
+@bot.on(admin_cmd(pattern=f"زين$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"زين$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 2
+    animation_ttl = range(6)
+    event = await edit_or_reply(event, "᷂أهـلُِآً ᷂صدِيقٌي ᷂ֆ☁️💙؛")
+    animation_chars = ["😁🏿", "😁🏾", "😁🏽", "😁🏼", "‎😁", "**᷂بخير ᷂بشوفتك ᷂صديقي ᷂؛ ま**"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 6])
+
+
+@bot.on(admin_cmd(pattern=f"monkey$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"monkey$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 2
+    animation_ttl = range(12)
+    event = await edit_or_reply(event, "Hey There....")
+    animation_chars = ["🐵", "🙉", "🙈", "🙊", "🖕‎🐵🖕", "**Good to see you friend....**"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 6])
+
+
+@bot.on(admin_cmd(pattern=f"hand$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"hand$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1
+    animation_ttl = range(13)
+    event = await edit_or_reply(event, "🖐️")
+    animation_chars = [
+        "👈",
+        "👉",
+        "☝️",
+        "👆",
+        "🖕",
+        "👇",
+        "✌️",
+        "🤞",
+        "🖖",
+        "🤘",
+        "🤙",
+        "🖐️",
+        "👌",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 13])
+
+
+@bot.on(admin_cmd(pattern=f"gsg$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"gsg$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1
+    animation_ttl = range(12)
+    event = await edit_or_reply(event, "ContDown....")
+    animation_chars = [
+        "🔟",
+        "9️⃣",
+        "8️⃣",
+        "7️⃣",
+        "6️⃣",
+        "5️⃣",
+        "4️⃣",
+        "3️⃣",
+        "2️⃣",
+        "1️⃣",
+        "0️⃣",
+        "🆘",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 12])
+
+
+@bot.on(admin_cmd(pattern=r"theart$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"theart$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(54)
+    event = await edit_or_reply(event, "🖤")
+    animation_chars = [
+        "❤️",
+        "🧡",
+        "💛",
+        "💚",
+        "💙",
+        "💜",
+        "🖤",
+        "💘",
+        "💝",
+        "❤️",
+        "🧡",
+        "💛",
+        "💚",
+        "💙",
+        "💜",
+        "🖤",
+        "💘",
+        "💝",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 18])
+
+
+CMD_HELP.update(
+    {
+        "متحركات الخامسة": """**Plugin : **`متحركات الخامسة`
+        
+**Commands in animation5 are **
+  •  `جاري`
+  •  `شباك`
+  •  `up`
+  •  `طوبة`
+  •  `قلب`
+  •  `مكتئب`
+  •  `زين`
+  •  `.monkey`
+  •  `.herber`
+  •  `.hand`
+  •  `.gsg`
+  •  `.theart`
+  
+**Function : **__Different kinds of animation commands check yourself for their animation .__"""
+    }
+)
